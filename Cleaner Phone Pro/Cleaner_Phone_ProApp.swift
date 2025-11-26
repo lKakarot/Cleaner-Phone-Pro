f0d0f74ff3048f244dd5167aba86ced9276dadd6
SALHI Younes <younes-du-27@hotmail.fr>
2025-11-26 17:26:53 +0100

    Initial Commit

---
+++ Cleaner Phone Pro/Cleaner_Phone_ProApp.swift
@@ -0,0 +1,17 @@
+//
+//  Cleaner_Phone_ProApp.swift
+//  Cleaner Phone Pro
+//
+//  Created by Hasnae HANY on 26/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Cleaner_Phone_ProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Cleaner Phone Pro/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Cleaner Phone Pro
+//
+//  Created by Hasnae HANY on 26/11/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
